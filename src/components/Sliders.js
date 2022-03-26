773a499b231bd78984fb337b7daa8ee92fa42568
patthekid99 <pattyvan4@gmail.com>
2022-03-26 14:20:10 -0700

    worked on slider text still need to fix buttons

--- src/components/Sliders.js
+++ src/components/Sliders.js
@@ -1,5 +1,6 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "../styles/styles.scss";
 import React from "react";
 import Slider from "react-slick";
 
@@ -11,34 +12,37 @@ import slider3 from '../images/header-slider-img-03.jpg';
 function Sliders(){
 
     var settings = {
-      opacity: 0,
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
       };
 
     return(
-        <Slider {...settings}>
-              <div>
-                <img src={slider1} alt="slider1" />
-              </div>
-              <div>
-                <img src={slider2} alt="slider2" />
-              </div>
-              <div>
-                <img src={slider3} alt="slider3" />
-              </div>
-
-
-              {/* This is not working yet */}
-              <div class="detail">
-                  <h2>We design and fabricate indestructible wastewater screens. Enough said.</h2>					
-                  <a href="" class="view-products">VIEW PRODUCTS</a>
-						      <a href="" class="view-services">VIEW SERVICES</a>
-					    </div>                   
-          </Slider>
+      <>
+      <Slider {...settings}>
+          <div className="slider" >
+            <img src={slider1} alt="slider2" />
+            <h2 className="slider-label">WE DESIGN AND FABRICATE INDESTRUCTIBLE WASTEWATER SCREENS. ENOUGH SAID.</h2>
+            <button className="slider-btn">VIEW PRODUCTS</button>
+            <button className="slider-btn">VIEW SERVICES</button>
+          </div>
+          <div className="slider">
+            <img src={slider2} alt="slider2" />
+            <h2 className="slider-label">WE DESIGN AND FABRICATE INDESTRUCTIBLE WASTEWATER SCREENS. ENOUGH SAID.</h2>
+            <button className="slider-btn">VIEW PRODUCTS</button>
+            <button className="slider-btn">VIEW SERVICES</button>
+          </div>
+          <div className="slider">
+            <img src={slider3} alt="slider3" />
+            <h2 className="slider-label">WE DESIGN AND FABRICATE INDESTRUCTIBLE WASTEWATER SCREENS. ENOUGH SAID.</h2>
+            <button className="slider-btn">VIEW PRODUCTS</button>
+            <button className="slider-btn">VIEW SERVICES</button>
+          </div>
+        </Slider></>
 
     )
 }
